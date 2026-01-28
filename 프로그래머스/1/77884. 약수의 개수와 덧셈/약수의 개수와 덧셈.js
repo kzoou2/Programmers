96e6140aa8ab1104e0fa1269655dfbee802b8c01
Kyung Joo <137479578+kzoou2@users.noreply.github.com>
2026-01-28 23:42:24 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,14 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for (let i = left; i <= right; i++){
+        
+        if(Number.isInteger(Math.sqrt(i))){
+            answer -= i;
+        } else{
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
